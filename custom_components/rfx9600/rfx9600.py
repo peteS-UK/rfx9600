65c87395838f8ff111f112fa265eb8ac158104ca
peteS-UK <peter@sketch.cc>
2025-01-07 23:26:09 +0000

    Code tidy

--- custom_components/rfx9600/rfx9600.py
+++ custom_components/rfx9600/rfx9600.py
@@ -1,169 +1,160 @@
-
 import logging
-
-import socket
-
-from lxml import etree
 
 import asyncio
 
 import asyncio_datagram
-
-import time
 
 import sys
 
 # Third byte is a sequence number
 # third from last is off/on
 # fourth from last is relay number
-RELAY_COMMAND = bytearray(b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x63\x00\x00\x06\x00\x00\x00\x00")
+RELAY_COMMAND = bytearray(
+    b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x63\x00\x00\x06\x00\x00\x00\x00"
+)
 
 # fourth from last is relay number
-RELAY_TOGGLE = bytearray(b"\x00\x00\x08\x00\x00\x00\x00\x00\x00\x00\x00\x00\x64\x00\x00\x05\x00\x00\x00\x00")
+RELAY_TOGGLE = bytearray(
+    b"\x00\x00\x08\x00\x00\x00\x00\x00\x00\x00\x00\x00\x64\x00\x00\x05\x00\x00\x00\x00"
+)
 
 # fourth from last is relay number
-RELAY_QUERY = bytearray(b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x65\x00\x00\x05\x00\x00\x00\x00")
+RELAY_QUERY = bytearray(
+    b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x65\x00\x00\x05\x00\x00\x00\x00"
+)
 
 _LOGGER = logging.getLogger(__name__)
 
+
 class RFX9600(object):
-	def __init__(self, ip, name):
+    def __init__(self, ip, name):
+        self._ip = ip
+        self._name = name
+        self._port = 65442
+        self._port_name = []
+        self.state = False
+        self._seq = 0
 
-		self._ip = ip
-		self._name = name
-		self._port = 65442
-		self._port_name = []
-		self.state = False
-		self._seq = 0
+    async def async_udp_connect(self):
+        _stream = None
+        try:
+            _stream = await asyncio_datagram.connect((self._ip, self._port))
+        except IOError as e:
+            _LOGGER.critical(
+                "Cannot connect command socket %d: %s", e.errno, e.strerror
+            )
+        except Exception:
+            _LOGGER.critical(
+                "Unknown error on command socket connection %s", sys.exc_info()[0]
+            )
+        return _stream
 
-	async def async_udp_connect(self):
+    async def async_udp_disconnect(self, _stream):
+        try:
+            _stream.close()
+        except IOError as e:
+            _LOGGER.critical(
+                "Cannot disconnect from command socket %d: %s", e.errno, e.strerror
+            )
+        except Exception:
+            _LOGGER.critical(
+                "Unknown error on command socket disconnection %s", sys.exc_info()[0]
+            )
 
-		_stream = None
-		try:
-			_stream = await asyncio_datagram.connect((self._ip, self._port))
-		except IOError as e:
-			_LOGGER.critical("Cannot connect command socket %d: %s", e.errno, e.strerror)
-		except:
-			_LOGGER.critical("Unknown error on command socket connection %s", sys.exc_info()[0])
-		return _stream
+    async def async_turn_on(self, port_number):
+        command = RELAY_COMMAND
+        command[-4] = port_number - 1
+        command[-3] = 1
 
-	async def async_udp_disconnect(self, _stream):
-		try:
-			_stream.close()
-		except IOError as e:
-			_LOGGER.critical("Cannot disconnect from command socket %d: %s", e.errno, e.strerror)
-		except:
-			_LOGGER.critical("Unknown error on command socket disconnection %s", sys.exc_info()[0])
+        if self._seq == 16777215:
+            self._seq = 0
+        else:
+            self._seq = self._seq + 1
 
-	async def async_turn_on(self, port_number):
+        _LOGGER.debug("Turn On relay_%d with seq %d", port_number, self._seq)
 
-		command = RELAY_COMMAND
-		command[-4] = port_number - 1
-		command[-3] = 1
+        _seq_bytes = self._seq.to_bytes(3)
+        command[0] = _seq_bytes[0]
+        command[1] = _seq_bytes[1]
+        command[2] = _seq_bytes[2]
 
+        await self._async_send_command(bytes(command), port_number, self._seq)
 
-		if self._seq == 16777215:
-			self._seq = 0
-		else:
-			self._seq = self._seq+1
+    async def async_turn_off(self, port_number):
+        command = RELAY_COMMAND
 
-		_LOGGER.debug("Turn On relay_%d with seq %d", port_number, self._seq)
+        command[-4] = port_number - 1
+        command[-3] = 0
 
-		_seq_bytes = self._seq.to_bytes(3)
-		command[0] = _seq_bytes[0]
-		command[1] = _seq_bytes[1]
-		command[2] = _seq_bytes[2]
-		
-		await self._async_send_command(bytes(command), port_number,  self._seq)
+        if self._seq == 16777215:
+            self._seq = 0
+        else:
+            self._seq = self._seq + 1
 
+        _LOGGER.debug("Turn Off relay_%d with seq %d", port_number, self._seq)
 
+        _seq_bytes = self._seq.to_bytes(3)
+        command[0] = _seq_bytes[0]
+        command[1] = _seq_bytes[1]
+        command[2] = _seq_bytes[2]
 
+        await self._async_send_command(bytes(command), port_number, self._seq)
 
+    async def async_update(self, port_number):
+        command = RELAY_QUERY
+        command[-4] = port_number - 1
 
-	async def async_turn_off(self, port_number):
+        if self._seq == 16777215:
+            self._seq = 0
+        else:
+            self._seq = self._seq + 1
 
+        _LOGGER.debug("Calling update for relay_%d with seq %d", port_number, self._seq)
 
-		command = RELAY_COMMAND
-		
-		command[-4] = port_number - 1
-		command[-3] = 0
+        _seq_bytes = self._seq.to_bytes(3)
+        command[0] = _seq_bytes[0]
+        command[1] = _seq_bytes[1]
+        command[2] = _seq_bytes[2]
 
+        await self._async_send_command(bytes(command), port_number, self._seq, True)
 
-		if self._seq == 16777215:
-			self._seq = 0
-		else:
-			self._seq = self._seq+1
+        if self._resp:
+            if self._resp[12] == 0:
+                _LOGGER.debug("Setting state to False for port number %d", port_number)
+                self.state = False
+            if self._resp[12] == 1:
+                _LOGGER.debug("Setting state to True for port number %d", port_number)
+                self.state = True
 
-		_LOGGER.debug("Turn Off relay_%d with seq %d", port_number, self._seq)
+    async def _async_send_command(self, command, port_number, _seq, ack=False):
+        _stream = await self.async_udp_connect()
 
-		_seq_bytes = self._seq.to_bytes(3)
-		command[0] = _seq_bytes[0]
-		command[1] = _seq_bytes[1]
-		command[2] = _seq_bytes[2]
+        if not _stream:
+            return
 
-		await self._async_send_command(bytes(command), port_number, self._seq)
+        await _stream.send(command)
 
+        if ack:
+            try:
+                _data, remote_addr = await asyncio.wait_for(_stream.recv(), timeout=0.2)
+                await asyncio.sleep(0.1)
+                _resp, remote_addr = await asyncio.wait_for(_stream.recv(), timeout=0.2)
+            except Exception:
+                _LOGGER.debug(
+                    "No query response received for port %d, seq %d", port_number, _seq
+                )
+                self._resp = None
+                return
 
+            if (
+                _resp[0] == command[0]
+                and _resp[1] == command[1]
+                and _resp[2] == command[2]
+                and _resp[3] == 64
+            ):
+                _LOGGER.debug(
+                    "Query response received for port %d, seq %d", port_number, _seq
+                )
+                self._resp = _resp
 
-	async def async_update(self, port_number):
-
-		command = RELAY_QUERY
-		command[-4] = port_number - 1
-
-		if self._seq == 16777215:
-			self._seq = 0
-		else:
-			self._seq = self._seq+1
-
-		_LOGGER.debug("Calling update for relay_%d with seq %d", port_number, self._seq)
-
-		_seq_bytes = self._seq.to_bytes(3)
-		command[0] = _seq_bytes[0]
-		command[1] = _seq_bytes[1]
-		command[2] = _seq_bytes[2]
-		
-		await self._async_send_command(bytes(command), port_number, self._seq, True)
-
-		if self._resp:
-			if self._resp[12] == 0:
-				_LOGGER.debug("Setting state to False for port number %d", port_number)
-				self.state = False
-			if self._resp[12] == 1:
-				_LOGGER.debug("Setting state to True for port number %d", port_number)
-				self.state = True
-
-
-
-	async def _async_send_command(self, command, port_number, _seq, ack = False):
-
-		_stream = await self.async_udp_connect()
-
-		if not _stream:
-			return
-
-		await _stream.send(command)
-
-#		try: 
-#			await _stream.send(command)
-#		except IOError as e:
-#			_LOGGER.critical("Cannot send to command socket %d: %s", e.errno, e.strerror)
-#		except:
-#			_LOGGER.critical("Unknown error on command socket send %s", sys.exc_info()[0])
-
-		if ack:
-			try:
-				_data, remote_addr = await asyncio.wait_for(_stream.recv(), timeout=0.2)
-				await asyncio.sleep(0.1)
-				_resp, remote_addr = await asyncio.wait_for(_stream.recv(), timeout=0.2)
-			except:
-				_LOGGER.debug("No query response received for port %d, seq %d",port_number, _seq )
-				self._resp = None
-				return
-			
-			if (_resp[0] == command [0] and _resp[1] == command [1] 
-	   				and _resp[2] == command [2] and _resp[3] == 64):
-					_LOGGER.debug("Query response received for port %d, seq %d",port_number, _seq)
-					self._resp = _resp
-
-		await self.async_udp_disconnect(_stream)
-		
+        await self.async_udp_disconnect(_stream)
--- custom_components/rfx9600/switch.py
+++ custom_components/rfx9600/switch.py
@@ -1,164 +1,137 @@
-
 from __future__ import annotations
 
+import asyncio
 import logging
-
-from .const import DOMAIN
-
-from .rfx9600 import RFX9600
+from datetime import timedelta
 
 import voluptuous as vol
-
-import asyncio
-
 from homeassistant import config_entries, core
-
-from homeassistant.components.switch import (
-	PLATFORM_SCHEMA,
-	SwitchEntity
-	)
-
+from homeassistant.components.switch import PLATFORM_SCHEMA, SwitchEntity
 from homeassistant.const import CONF_HOST, CONF_NAME
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers import (
-	config_validation as cv,
-	discovery_flow,
-	entity_platform,
-)
-
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.start import async_at_start
 
-_LOGGER = logging.getLogger(__name__)
-
 from .const import (
-	DOMAIN,
-	CONF_RELAY_1,
-	CONF_RELAY_2,
-	CONF_RELAY_3,
-	CONF_RELAY_4,
-	DEFAULT_NAME
+    CONF_RELAY_1,
+    CONF_RELAY_2,
+    CONF_RELAY_3,
+    CONF_RELAY_4,
+    DOMAIN,
 )
 
+_LOGGER = logging.getLogger(__name__)
+
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
-	{vol.Optional(CONF_RELAY_1, default="Relay 1"): cv.string,
-	vol.Optional(CONF_RELAY_2, default="Relay 2"): cv.string,
-	vol.Optional(CONF_RELAY_3, default="Relay 3"): cv.string,
-	vol.Optional(CONF_RELAY_4, default="Relay 4"): cv.string,
-	vol.Required(CONF_HOST): cv.string, 
-	vol.Optional(CONF_NAME): cv.string
-}
+    {
+        vol.Optional(CONF_RELAY_1, default="Relay 1"): cv.string,
+        vol.Optional(CONF_RELAY_2, default="Relay 2"): cv.string,
+        vol.Optional(CONF_RELAY_3, default="Relay 3"): cv.string,
+        vol.Optional(CONF_RELAY_4, default="Relay 4"): cv.string,
+        vol.Required(CONF_HOST): cv.string,
+        vol.Optional(CONF_NAME): cv.string,
+    }
 )
 
 PARALLEL_UPDATES = 1
 
-from datetime import timedelta
-
 SCAN_INTERVAL = timedelta(seconds=300)
 
+
 async def async_setup_entry(
-	hass: core.HomeAssistant,
-	config_entry: config_entries.ConfigEntry,
-	async_add_entities,
+    hass: core.HomeAssistant,
+    config_entry: config_entries.ConfigEntry,
+    async_add_entities,
 ) -> None:
+    config = hass.data[DOMAIN][config_entry.entry_id]
 
-	config = hass.data[DOMAIN][config_entry.entry_id]
+    rfx9600 = config["rfx9600"]
 
-	rfx9600 = config["rfx9600"]
-	
-	relays = []
+    relays = []
 
-	for port in range(1,5):
-		port_name = "relay_"+str(port)
-		relays.append(RFX9600Device(rfx9600, rfx9600._port_name[port-1], port, hass))
+    for port in range(1, 5):
+        relays.append(RFX9600Device(rfx9600, rfx9600._port_name[port - 1], port, hass))
 
-	async_add_entities(relays)
+    async_add_entities(relays)
+
 
 class RFX9600Device(SwitchEntity):
-	# Representation of a RFX9600
+    # Representation of a RFX9600
 
-	def __init__(self, device, port_name, port_number, hass):
+    def __init__(self, device, port_name, port_number, hass):
+        self._device = device
+        self._port_name = port_name
+        self._port_number = port_number
+        # self._udp_stream = device._udp_stream
+        self._hass = hass
+        self._entity_id = "switch.rfx9600_relay_" + str(port_number)
+        self._unique_id = "rfx9600_relay_" + str(port_number)
 
-		self._device = device
-		self._port_name = port_name
-		self._port_number = port_number
-		# self._udp_stream = device._udp_stream
-		self._hass = hass
-		self._entity_id = "switch.rfx9600_relay_" + str(port_number)
-		self._unique_id = "rfx9600_relay_"+str(port_number)
+    async def _async_startup(self, loop):
+        # await self._device.async_udp_connect(self._port_number)
+        await self._device.async_update(self._port_number)
 
-	async def _async_startup(self, loop):
-		#await self._device.async_udp_connect(self._port_number)
-		await self._device.async_update(self._port_number)
+    async def async_added_to_hass(self):
+        # await self._device.async_udp_connect(self._port_number)
+        # await self._device.async_update(self._port_number)
+        async_at_start(self._hass, self._async_startup)
 
-	async def async_added_to_hass(self):
-		#await self._device.async_udp_connect(self._port_number)
-		#await self._device.async_update(self._port_number)
-		async_at_start(self._hass,  self._async_startup)
+    async def async_will_remove_from_hass(self) -> None:
+        pass
+        # await self._device.async_udp_disconnect(self._port_number)
 
-	async def async_will_remove_from_hass(self) -> None:
-		
-		pass
-		#await self._device.async_udp_disconnect(self._port_number)
+    @property
+    def should_poll(self):
+        return True
 
+    @property
+    def name(self):
+        return self._port_name
 
-	should_poll = True
+    @property
+    def has_entity_name(self):
+        return True
 
-	@property
-	def should_poll(self):
-		return True
+    @property
+    def device_info(self) -> DeviceInfo:
+        """Return the device info."""
+        return DeviceInfo(
+            identifiers={
+                # Serial numbers are unique identifiers within a specific domain
+                (DOMAIN, self._device._name)
+            },
+            name=self._device._name,
+            manufacturer="Philips",
+            model="RFX9600",
+        )
 
+    @property
+    def unique_id(self):
+        return self._unique_id
 
+    @property
+    def entity_id(self):
+        return self._entity_id
 
-	@property
-	def name(self):
-		return self._port_name
+    @entity_id.setter
+    def entity_id(self, entity_id):
+        self._entity_id = entity_id
 
-	@property
-	def has_entity_name(self):
-		return True
+    @property
+    def is_on(self):
+        return self._device.state
 
-	@property
-	def device_info(self) -> DeviceInfo:
-		"""Return the device info."""
-		return DeviceInfo(
-			identifiers={
-				# Serial numbers are unique identifiers within a specific domain
-				(DOMAIN, self._device._name)
-			},
-			name=self._device._name,
-			manufacturer='Philips',
-			model="RFX9600")
+    async def async_turn_on(self, **kwargs):
+        await self._device.async_turn_on(self._port_number)
+        await asyncio.sleep(1)
+        self._device.state = True
+        self.async_schedule_update_ha_state(force_refresh=False)
 
-	@property
-	def unique_id(self):
-		return self._unique_id
-		
-	@property
-	def entity_id(self):
-		return self._entity_id
-	
-	@entity_id.setter
-	def entity_id(self, entity_id):
-		self._entity_id = entity_id
+    async def async_turn_off(self, **kwargs):
+        await self._device.async_turn_off(self._port_number)
+        await asyncio.sleep(1)
+        self._device.state = False
+        self.async_schedule_update_ha_state(force_refresh=False)
 
-	@property
-	def is_on(self):
-		return self._device.state
-
-	async def async_turn_on(self, **kwargs):
-		await self._device.async_turn_on(self._port_number)
-		await asyncio.sleep(1)
-		self._device.state = True
-		self.async_schedule_update_ha_state(force_refresh=False)
-
-	async def async_turn_off(self, **kwargs):
-		await self._device.async_turn_off(self._port_number)
-		await asyncio.sleep(1)
-		self._device.state = False
-		self.async_schedule_update_ha_state(force_refresh=False)
-
-	async def async_update(self):
-		await self._device.async_update(self._port_number)
+    async def async_update(self):
+        await self._device.async_update(self._port_number)
