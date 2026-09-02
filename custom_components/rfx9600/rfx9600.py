196f692f83db8a3ed309b0536ab08b77c28e9183
peteS-UK <64092177+peteS-UK@users.noreply.github.com>
2026-09-02 17:53:30 +0100

    rs232 support

--- custom_components/rfx9600/rfx9600.py
+++ custom_components/rfx9600/rfx9600.py
@@ -1,10 +1,9 @@
-import logging
-
 import asyncio
+import codecs
+import logging
+import sys
 
 import asyncio_datagram
-
-import sys
 
 # Third byte is a sequence number
 # third from last is off/on
@@ -26,7 +25,7 @@ RELAY_QUERY = bytearray(
 _LOGGER = logging.getLogger(__name__)
 
 
-class RFX9600(object):
+class RFX9600:
     def __init__(self, ip, name):
         self._ip = ip
         self._name = name
@@ -35,11 +34,24 @@ class RFX9600(object):
         self.state = False
         self._seq = 0
 
+    def _next_sequence(self):
+        if self._seq == 16777215:
+            self._seq = 0
+        else:
+            self._seq = self._seq + 1
+        return self._seq
+
+    def _set_sequence(self, command):
+        _seq_bytes = self._seq.to_bytes(3)
+        command[0] = _seq_bytes[0]
+        command[1] = _seq_bytes[1]
+        command[2] = _seq_bytes[2]
+
     async def async_udp_connect(self):
         _stream = None
         try:
             _stream = await asyncio_datagram.connect((self._ip, self._port))
-        except IOError as e:
+        except OSError as e:
             _LOGGER.critical(
                 "Cannot connect command socket %d: %s", e.errno, e.strerror
             )
@@ -52,7 +64,7 @@ class RFX9600(object):
     async def async_udp_disconnect(self, _stream):
         try:
             _stream.close()
-        except IOError as e:
+        except OSError as e:
             _LOGGER.critical(
                 "Cannot disconnect from command socket %d: %s", e.errno, e.strerror
             )
@@ -66,17 +78,11 @@ class RFX9600(object):
         command[-4] = port_number - 1
         command[-3] = 1
 
-        if self._seq == 16777215:
-            self._seq = 0
-        else:
-            self._seq = self._seq + 1
+        self._next_sequence()
 
         _LOGGER.debug("Turn On relay_%d with seq %d", port_number, self._seq)
 
-        _seq_bytes = self._seq.to_bytes(3)
-        command[0] = _seq_bytes[0]
-        command[1] = _seq_bytes[1]
-        command[2] = _seq_bytes[2]
+        self._set_sequence(command)
 
         await self._async_send_command(bytes(command), port_number, self._seq)
 
@@ -86,17 +92,11 @@ class RFX9600(object):
         command[-4] = port_number - 1
         command[-3] = 0
 
-        if self._seq == 16777215:
-            self._seq = 0
-        else:
-            self._seq = self._seq + 1
+        self._next_sequence()
 
         _LOGGER.debug("Turn Off relay_%d with seq %d", port_number, self._seq)
 
-        _seq_bytes = self._seq.to_bytes(3)
-        command[0] = _seq_bytes[0]
-        command[1] = _seq_bytes[1]
-        command[2] = _seq_bytes[2]
+        self._set_sequence(command)
 
         await self._async_send_command(bytes(command), port_number, self._seq)
 
@@ -104,17 +104,11 @@ class RFX9600(object):
         command = RELAY_QUERY
         command[-4] = port_number - 1
 
-        if self._seq == 16777215:
-            self._seq = 0
-        else:
-            self._seq = self._seq + 1
+        self._next_sequence()
 
         _LOGGER.debug("Calling update for relay_%d with seq %d", port_number, self._seq)
 
-        _seq_bytes = self._seq.to_bytes(3)
-        command[0] = _seq_bytes[0]
-        command[1] = _seq_bytes[1]
-        command[2] = _seq_bytes[2]
+        self._set_sequence(command)
 
         await self._async_send_command(bytes(command), port_number, self._seq, True)
 
@@ -126,6 +120,87 @@ class RFX9600(object):
                 _LOGGER.debug("Setting state to True for port number %d", port_number)
                 self.state = True
 
+    async def async_send_rs232(
+        self,
+        command_string,
+        port_number,
+        baud_rate,
+        stop_bits,
+        parity,
+        data_bits,
+        duration_ms=500,
+        repeat_count=1,
+    ):
+        baud_lookup = {
+            2400: 0x2,
+            4800: 0x3,
+            9600: 0x4,
+            14400: 0x5,
+            19200: 0x6,
+            28800: 0x7,
+            31250: 0x8,
+            38400: 0x9,
+            57600: 0xA,
+            115200: 0xB,
+        }
+        stop_bits_lookup = {1: 0x0, 1.5: 0x2, 2: 0x4}
+        parity_lookup = {
+            "none": 0x0,
+            "even": 0x8,
+            "odd": 0x10,
+            "mark": 0x18,
+            "space": 0x20,
+        }
+        data_bits_lookup = {5: 0x00, 6: 0x40, 7: 0x80, 8: 0xC0}
+
+        baud_code = baud_lookup[baud_rate]
+        stop_code = stop_bits_lookup[stop_bits]
+        parity_code = parity_lookup[parity]
+        data_bits_code = data_bits_lookup[data_bits]
+
+        port_baud = ((port_number - 1) << 4) | baud_code
+        rs232_flags = stop_code + parity_code + data_bits_code
+
+        decoded_command = codecs.decode(command_string, "unicode_escape")
+        command_bytes = decoded_command.encode("ascii")
+        if len(command_bytes) > 236:
+            raise ValueError("RS232 command is too long (maximum 236 bytes)")
+
+        payload = bytearray()
+        payload.append(port_baud)
+        payload.append(rs232_flags)
+        payload.extend(duration_ms.to_bytes(2, byteorder="big", signed=False))
+        payload.extend(b"\x00\x00\x00\x00\x00\x00\x00")
+        payload.append(repeat_count)
+        payload.extend(command_bytes)
+        payload.extend(b"\x00\x00\x00")
+
+        rs232_command = bytearray(b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00")
+        rs232_command.append(0x50)
+        rs232_command.append(0x02)
+        rs232_command.append(0x00)
+        data_length = len(payload) + 4
+        rs232_command.append(data_length)
+        rs232_command.extend(payload)
+
+        self._next_sequence()
+        self._set_sequence(rs232_command)
+
+        payload_hex = " ".join(f"{byte:02X}" for byte in payload)
+        frame_hex = " ".join(f"{byte:02X}" for byte in rs232_command)
+        _LOGGER.debug(
+            "Sending RS232 command on port %d with seq %d: data_length=%d command=%r decoded_command=%r payload_hex=%s frame_hex=%s",
+            port_number,
+            self._seq,
+            data_length,
+            command_string,
+            decoded_command,
+            payload_hex,
+            frame_hex,
+        )
+
+        await self._async_send_command(bytes(rs232_command), port_number, self._seq)
+
     async def _async_send_command(self, command, port_number, _seq, ack=False):
         _stream = await self.async_udp_connect()
 
@@ -136,9 +211,13 @@ class RFX9600(object):
 
         if ack:
             try:
-                _data, remote_addr = await asyncio.wait_for(_stream.recv(), timeout=0.2)
+                _data, _remote_addr = await asyncio.wait_for(
+                    _stream.recv(), timeout=0.2
+                )
                 await asyncio.sleep(0.1)
-                _resp, remote_addr = await asyncio.wait_for(_stream.recv(), timeout=0.2)
+                _resp, _remote_addr = await asyncio.wait_for(
+                    _stream.recv(), timeout=0.2
+                )
             except Exception:
                 _LOGGER.debug(
                     "No query response received for port %d, seq %d", port_number, _seq
